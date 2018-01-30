f0fb21ae83bc1373013fdf2844d67f967069efe7
mistim <m.mistim@gmail.com>
2018-01-30 15:45:44 +0200

    - ex_doc deps;

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule Materialize.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:ex_doc, "~> 0.14", only: :docs},
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:phoenix, "~> 1.3.0"}
     ]
   end
